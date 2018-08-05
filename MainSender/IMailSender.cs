1e044f6b26cd1bebd9aa90917d847c28f8c4a222
JennyRockU <jenny@tipranks.com>
2018-08-05 19:04:23 +0300

    Initial upload of MainSender project
    This project files are shared by other projects (the SendGridSender and MailgunSender worker roles)

---
+++ MainSender/IMailSender.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.ApplicationInsights;
+using SendEmailApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MainSender
+{
+    public interface IMailSender
+    {
+        string ApiKey { get; }
+        Task<bool> SendEmail(EmailDetails msg,
+            TelemetryClient telelemtry,
+            CancellationToken cancelation = new CancellationToken());
+    }
+
+    public class EmailMessage
+    {
+        public EmailMessage(KeyValuePair<string, string> sender,
+            Dictionary<string, string> recipeints,
+            string subject,
+            string body)
+        {
+            this.Sender = sender;
+            this.Recipeints = recipeints;
+            this.Subject = subject;
+            this.Body = body;
+        }
+
+        public KeyValuePair<string, string> Sender { get; set; }
+        public Dictionary<string, string> Recipeints { get; set; }
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ MainSender/MessageHandler.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MainSender
+{
+    public class MessageHandler
+    {
+        private static readonly string AppInsightsKey = ConfigurationManager.AppSettings["instrumentalKey"];
+        private static readonly TelemetryClient telemetry = new TelemetryClient { InstrumentationKey = AppInsightsKey };
+        
+        public static async Task<bool> ProcessMessage(CloudQueueMessage queueMsg,
+            IMailSender emailVendor,
+            CancellationToken cancellationToken)
+        {
+            const int maxDequeue = 5;
+
+            try
+            {
+                // check whether a message was pulled out and returned to the queue five times
+                if (queueMsg.DequeueCount >= maxDequeue)
+                {
+                    // if that's that case, report and return
+                    telemetry.TrackTrace($"Skipping message after {maxDequeue} dequeues. " +
+                        $"Message: {queueMsg?.AsString}");
+
+                    // return 'true' in order for the message will be deleted by the Worker Role
+                    return true;
+                }
+
+                var emailDetails = SendEmailApi.EmailDetails.UnwrapMessage(queueMsg.AsString);
+                return await emailVendor.SendEmail(emailDetails, telemetry, cancellationToken);
+
+            } catch (Exception e)
+            {
+                //should not happen as there is a catch inside
+                telemetry.TrackException(e, 
+                    new Dictionary<string, string> { { emailVendor.GetType().Name, queueMsg?.AsString} });
+
+                return false;
+            }
+    
+        }
+    }
+}
