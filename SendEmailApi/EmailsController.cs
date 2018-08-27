1f4a5d987669dbff03b5426f2c20b596d4e9edd5
JennyRockU <jennyrukman@gmail.com>
2018-08-27 21:40:00 +0300

    Added authentication

--- SendEmailApi/EmailsController.cs
+++ SendEmailApi/EmailsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,16 @@ using System.Threading.Tasks;
 using System.Web.Configuration;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using static SendEmailApi.WebApiConfig;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 
 namespace SendEmailApi.Controllers
 {
+    [BasicAuthentication]
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class EmailsController : ApiController
     {
+        // exception tracking telemetry client
         private static string AppInsightsKey = WebConfigurationManager.AppSettings["appInsightsInstrumentalKey"];
         private static readonly TelemetryClient telemetry = new TelemetryClient { InstrumentationKey = AppInsightsKey };
 
@@ -35,20 +40,24 @@ namespace SendEmailApi.Controllers
                 return BadRequest("Failed to get email content");
             }
 
-            if(String.IsNullOrEmpty(to)) return BadRequest("Recipient email is missing (to)");
-            var emailDetails = new EmailDetails(to.Split(','), from, subject, content);
-            
+            if(string.IsNullOrEmpty(to)) return BadRequest("Recipient email is missing (to)");
+
+            var emailDetails = EmailDetails.CreateEmail(to.Split(',').ToList(), from, subject, content);
+
             if (!emailDetails.ValidRecipients) return BadRequest("Recipient email is either missing or not in a correct format");
+
             if (!emailDetails.ValidSender) return BadRequest("Sender email is either missing or not in a correct format");
+
             if (!emailDetails.ValidSubject) return BadRequest("Title exceeds allowed length");
         
             try
             {
-                 await EmailHandler.SendEmailToQueue(emailDetails);
+                 await EmailHandler.AddEmailToQueue(emailDetails);
+
             } catch
             {
                 //retry
-                await EmailHandler.SendEmailToQueue(emailDetails);
+                await EmailHandler.AddEmailToQueue(emailDetails);
             }
 
             return Ok("Email created");
