06c327312cc719915a80deb7a59dffa780c4bf79
JennyRockU <jennyrukman@gmail.com>
2018-08-27 21:29:47 +0300

    Removed message queue unwrapping tests (new tests were written in a separate class)

--- SendgridSenderRole/SendgridSendeUnitTests/MainSenderTests.cs
+++ SendgridSenderRole/SendgridSendeUnitTests/MainSenderTests.cs
@@ -1,4 +1,4 @@
-﻿using MainSender;
+using MainSender;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SendEmailApi;
 using System;
@@ -14,58 +14,17 @@ namespace MailgunSenderUnitTests
     {
 
         // organize the wanted/expected inputs for the tests
-        private static readonly string[] Recipients = new string[] { "jennyrukma@gmail.com", "jennyrocku@aol.com" };
+        private static readonly List<string> Recipients = new List<string> { "jennyrukman@gmail.com", "jennyrocku@aol.com" };
         private static readonly string SenderAddress = "john@doe.com";
         private static readonly string SubjectLine = "Test Email";
         private static readonly string EmailContent = "Hi, this email was sent via unit tests. Good luck!";
 
-        private static readonly EmailDetails EmailDetails = new EmailDetails(Recipients,
-             SenderAddress,
-             SubjectLine,
-              EmailContent);
+        private static EmailDetails EmailDetails => EmailDetails.CreateEmail(Recipients,
+                     SenderAddress,
+                     SubjectLine,
+                      EmailContent);
 
-        // the details' string the way it is stored in the queue
-        private static readonly string WrappedEmailDetails = $"<To>{string.Join(",",Recipients)}/To>" +
-            $"<From>{SenderAddress}</From>" +
-            $"<Subject>{SubjectLine}</Subject>" +
-            $"<Body>{EmailContent}</Body>";
         
-
-        //Test Unwrapping Each of the Email Details
-        [TestMethod]
-        public void TestUnwrappingRecipients()
-        {
-            var expectedRecipients = string.Join(",", Recipients);
-            var parsedRecipient = EmailDetails.GetTextWithTag("To", WrappedEmailDetails);
-
-            Assert.AreEqual(expectedRecipients, parsedRecipient);
-        }
-
-        [TestMethod]
-        public void TestUnwrappingSender()
-        {
-            var parsedSender = EmailDetails.GetTextWithTag("From", WrappedEmailDetails);
-
-            Assert.AreEqual(SenderAddress, parsedSender);
-        }
-
-        [TestMethod]
-        public void TestUnwrappingSubjectLine()
-        {
-            var parsedSubject = EmailDetails.GetTextWithTag("Subject", WrappedEmailDetails);
-
-            Assert.AreEqual(SubjectLine, parsedSubject);
-        }
-
-        [TestMethod]
-        public void TestUnwrappingEmailBody()
-        {
-            var parsedBody = EmailDetails.GetTextWithTag("Body", WrappedEmailDetails);
-
-            Assert.AreEqual(EmailContent, parsedBody);
-        }
-
-
         // a helper method to test the sending by the given email proviser
         // using a hard-coded email details
         public static async Task<bool> TestSendEmail(IMailSender emailSender)
