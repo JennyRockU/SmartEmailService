9cf52a4a4ce0d8811cb4a66d5b09fc042e588de0
JennyRockU <jennyrukman@gmail.com>
2018-08-27 21:34:13 +0300

    Removed methods and manual message unwrapping and removed constructor to support XML serialization

--- MainSender/EmailDetails.cs
+++ MainSender/EmailDetails.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,18 +7,24 @@ namespace SendEmailApi
 {
     public class EmailDetails
     {
-        public IEnumerable<string> Recipients;
+        public List<string> Recipients;
         public string Sender;
         public string Subject;
         public string Body;
 
-        public EmailDetails(IEnumerable<string> recipeints, string senderEmail, string subjectLine,
+        public static EmailDetails CreateEmail(List<string> recipeints,
+            string senderEmail,
+            string subjectLine,
             string emailBody)
         {
-            this.Recipients = recipeints.Select(x => x?.Trim());
-            this.Sender = senderEmail?.Trim();
-            this.Subject = subjectLine == null ? "" : subjectLine?.Trim();
-            this.Body = emailBody ?? "";
+            return new EmailDetails
+            {
+                Recipients = recipeints.Select(x => x?.Trim()).ToList(),
+                Sender = senderEmail?.Trim(),
+                Subject = subjectLine == null ? "" : subjectLine?.Trim(),
+                Body = emailBody ?? ""
+            };
+
         }
 
         public bool ValidRecipients => ValidateRecipients();
@@ -30,7 +36,7 @@ namespace SendEmailApi
         {
             const int maxAllowedRecipients = 50;
 
-            return Recipients.Count() > 0 && 
+            return Recipients.Count() > 0 &&
                    Recipients.Count() < maxAllowedRecipients &&
                    Recipients.Any(x => IsValidEmail(x));
         }
@@ -43,38 +49,7 @@ namespace SendEmailApi
             var emailParts = userEmail.Split('@').Count(x => !string.IsNullOrEmpty(x));
             return emailParts > 1;
         }
-
-        public string GetMessageQueueString()
-        {
-            return $"<To>{string.Join(",", Recipients)}</To>" +
-                $"<From>{Sender}</From>" +
-                $"<Subject>{Subject}</Subject>" +
-                $"<Body>{Body}</Body>";
-        }
-
-        public static EmailDetails UnwrapMessage(string msgFromQueue)
-        {
-            var recipients = GetTextWithTag("To", msgFromQueue);
-            var sender = GetTextWithTag("From", msgFromQueue);
-            var subject = GetTextWithTag("Subject", msgFromQueue);
-            var body = GetTextWithTag("Body", msgFromQueue);
-
-            return new EmailDetails(recipients.Split(','), sender, subject, body);
-
-        }
-
-        private static string GetTextWithTag(string tagName, string text)
-        {
-            var openingTagIndex = text.IndexOf($"<{tagName}>");
-            var lastIndexOfOpeningTag = text.Select((c, x) => new { Char = c, Index = x }).ToList().FindIndex(x => x.Index > openingTagIndex && x.Char.Equals('>'));
-            var startIndex = lastIndexOfOpeningTag + 1;
-            var closingTagIndex = text.IndexOf($"</{tagName}>");
-            var endIndex = closingTagIndex - startIndex;
-
-            return text.Substring(startIndex, endIndex);
-        }
+        
 
     }
-
-   
 }
